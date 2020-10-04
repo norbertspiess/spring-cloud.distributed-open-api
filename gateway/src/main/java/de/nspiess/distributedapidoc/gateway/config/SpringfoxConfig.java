f6b8c447b13b5f657acfbbd9261278192db25f8e
Norbert Spieß <norbertspiess@users.noreply.github.com>
2020-10-04 18:29:15 +0200

    provide solution for springfox

---
+++ gateway/src/main/java/de/nspiess/distributedapidoc/gateway/config/SpringfoxConfig.java
@@ -0,0 +1,41 @@
+package de.nspiess.distributedapidoc.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Configuration
+public class SpringfoxConfig {
+
+    @Bean
+    @Primary
+    public SwaggerResourcesProvider swaggerResourcesProvider(
+            DiscoveryClient discoveryClient,
+            @Value("${spring.application.name}") String gateway
+    ) {
+        return new SwaggerResourcesProvider() {
+            @Override
+            public List<SwaggerResource> get() {
+                return discoveryClient.getServices()
+                        .stream()
+                        .filter(service -> !service.equals(gateway))
+                        .map(service -> {
+                            var resource = new SwaggerResource();
+                            resource.setName(service);
+                            resource.setLocation(String.format("/%s/v3/api-docs", service));
+                            return resource;
+                        })
+                        .collect(toList());
+            }
+        };
+    }
+
+}
