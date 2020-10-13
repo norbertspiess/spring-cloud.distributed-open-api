69a9404ec4d547f34bc4dcea16d8ee532129b55d
Norbert Spieß <norbertspiess@users.noreply.github.com>
2020-10-13 10:29:22 +0200

    remove swagger annotations that are not available yet here

--- car/src/main/java/de/nspiess/distributeddocapi/car/controller/CarController.java
+++ car/src/main/java/de/nspiess/distributeddocapi/car/controller/CarController.java
@@ -1,14 +1,12 @@
 package de.nspiess.distributeddocapi.car.controller;
 
 import de.nspiess.distributeddocapi.car.controller.dto.Car;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
-@Tag(name = "Cars")
 public class CarController {
     @GetMapping
     public List<Car> getCars() {
--- trip/src/main/java/de/nspiess/distributeddocapi/car/controller/TripController.java
+++ trip/src/main/java/de/nspiess/distributeddocapi/car/controller/TripController.java
@@ -1,15 +1,12 @@
 package de.nspiess.distributeddocapi.car.controller;
 
 import de.nspiess.distributeddocapi.car.controller.dto.Trip;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/trips")
-@Tag(name = "Trips", description = "Trip API")
 public class TripController {
     @GetMapping
     public List<Trip> getTrips() {
@@ -17,7 +14,6 @@ public class TripController {
     }
 
     @PostMapping
-    @Operation(summary = "Create a trip")
     public Trip create(@RequestBody Trip trip) {
         return trip;
     }
